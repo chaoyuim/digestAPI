b7e2c0b2c2a6f40385372dd14bf067a12e901cc2
chao_yu <cyu@smart4solutions.nl>
2021-07-16 13:23:49 +0200

    t

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
-const https = require('https');
-const fs = require('fs');
-const app = require('express')();
+// const https = require('https');
+// const fs = require('fs');
+// const app = require('express')();
+// const auth = require('http-auth');
+
+// var digest = auth.digest({
+//     realm: 'Sample',
+//     file: __dirname + "/users.htdigest"
+// });
+// const options = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem')
+//   };
+
+// https.createServer(options,app);
+
+// app.get('/',digest.check((req,res)=>{
+//     console.log(req.auth);
+//     res.send({ username: 'whatever json' });
+// }));
+// // Levantamos servidor
+// app.listen(3001, () => {
+//     console.log('running');
+// });
+
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 const auth = require('http-auth');
 
 var digest = auth.digest({
     realm: 'Sample',
     file: __dirname + "/users.htdigest"
 });
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-  };
 
-https.createServer(options,app);
+var options = {
+  key: fs.readFileSync('.pem'),
+  cert: fs.readFileSync('.cert')
+};
+
+var app = express();
+app.use(digest.check());
+
+https.createServer(options, app)
 
 app.get('/',digest.check((req,res)=>{
     console.log(req.auth);
     res.send({ username: 'whatever json' });
 }));
-// Levantamos servidor
-app.listen(3001, () => {
-    console.log('running');
-});
 
-
-
-
-
+https.listen(3001);
 
 
 
