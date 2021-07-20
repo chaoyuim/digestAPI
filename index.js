fbf7fdebe1acf1fff0fb62d72b5f186a5baa78b6
chao_yu <cyu@smart4solutions.nl>
2021-07-20 20:54:48 +0200

    sa

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function() {
+app.listen(3000, 'localhost',function() {
   console.log('Hello World app listening on port 3000!');
 })
 
