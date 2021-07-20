70b01ee7bcbf33a13755e0a9b2caa1a79c2f8701
chao_yu <cyu@smart4solutions.nl>
2021-07-20 21:25:09 +0200

    s

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
-const express = require('express')
-const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-app.get('/app2', function (req, res) {
-    res.send('Hello World! app2')
-})
 
-app.listen(3000, 'localhost',function() {
-  console.log('Hello World app listening on port 3000!');
-})
+var express = require('express');
+var http = require('http');
+const auth = require('http-auth');
 
-// var express = require('express');
-// var http = require('http');
-// const auth = require('http-auth');
-
-// var digest = auth.digest({
-//     realm: 'Sample',
-//     file: __dirname + "/users.htdigest"
-// });
-// var app = express();
-// app.use(digest.check());
-// http.createServer(app);
-// app.get('/',digest.check((req,res)=>{
-//     console.log(req.auth);
-//     res.send({ username: 'whatever json' });
-// }));
-// app.listen(3001,()=>{
-//     console.log('running on port 30001');
-// });
+var digest = auth.digest({
+    realm: 'Sample',
+    file: __dirname + "/users.htdigest"
+});
+var app = express();
+app.use(digest.check());
+http.createServer(app);
+app.get('/digestapi',digest.check((req,res)=>{
+    console.log(req.auth);
+    res.send({ username: 'whatever json' });
+}));
+app.listen(3000,()=>{
+    console.log('running on port 30001');
+});
 
 
 
