053ecc3b4db42e1ed477860f7bb9b8dbe9e89b2a
chao_yu <cyu@smart4solutions.nl>
2021-07-21 10:58:30 +0200

    fix bug

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 var express = require('express');
 var http = require('http');
 const auth = require('http-auth');
@@ -9,15 +8,14 @@ var digest = auth.digest({
     file: __dirname + "/users.htdigest"
 });
 var app = express();
-app.use(digest.check());
-http.createServer(app);
+
+http.createServer(app,digest.check());
 app.get('/digestapi',digest.check((req,res)=>{
     console.log(req.auth);
     res.send({ username: 'whatever json' });
 }));
 app.listen(3000,()=>{
-    console.log('running on port 30001');
+    console.log('running on port 3000');
 });
-
 
 
